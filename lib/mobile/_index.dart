5cec31192ed413b2a9a98aa030e280557b67bdfb
MgKaung5254 <kmlinn.thaton@gmail.com>
2024-03-23 11:47:53 +0630

    Modified Project

--- lib/mobile/_index.dart
+++
@@ -1,8 +0,0 @@
-export 'ms_1_header.dart';
-export 'ms_2_about_me.dart';
-export 'ms_3_education.dart';
-export 'ms_4_experience.dart';
-export 'ms_5_volunteering.dart';
-export 'ms_6_technotes.dart';
-export 'ms_7_contact.dart';
-export 'ms_8_footer.dart';
--- lib/statics/_index.dart
+++
@@ -1,2 +0,0 @@
-export 'data_values.dart';
-export 'key_holders.dart';
